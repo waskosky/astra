ac6e14ef2a69dd6d21a630b1ef23c11f26408007
Nikhil Chavan <email@nikhilchavan.com>
2017-04-19 16:25:40 +0530

    Add simple welcome screen in the theme page

--- inc/core/view-general.php
+++ inc/core/view-general.php
@@ -16,41 +16,17 @@
 		<div id="post-body" class="metabox-holder">
 			<div id="postbox-container-2" class="postbox-container astra-row">
 
-				<div id="normal-sortables-1" class="meta-box-sortables ui-sortable astra-col-33">
+				<div id="normal-sortables-1" class="astra-col-33">
 				 	<div class="postbox ">
-						<button type="button" class="handlediv button-link" aria-expanded="true"><span class="screen-reader-text"><?php _e( 'Toggle panel: Clear Cache', 'astra' ); ?></span><span class="toggle-indicator" aria-hidden="true"></span></button>
-						<h2 class="ui-sortable-handle"><span><?php _e( 'Heading 1', 'astra' ); ?></span></h2>
+						<h2 class="ui-sortable-handle"><span><?php _e( 'Welcome to Astra', 'astra' ); ?></span></h2>
 						<div class="inside">
 							<p>
-								<?php _e( 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', 'astra' ); ?>
+								<?php _e( 'Astra is a very lightweight and beautiful theme made to work with Page Builders.', 'astra' ); ?>
 							</p>
-							<button class="button"><?php _e( 'Button 1', 'astra' ); ?></button>
-						</div>
-				 	</div>
-				</div>
-
-				<div id="normal-sortables-2" class="meta-box-sortables ui-sortable astra-col-33">
-				 	<div class="postbox ">
-						<button type="button" class="handlediv button-link" aria-expanded="true"><span class="screen-reader-text"><?php _e( 'Toggle panel: Clear Cache', 'astra' ); ?></span><span class="toggle-indicator" aria-hidden="true"></span></button>
-						<h2 class="ui-sortable-handle"><span><?php _e( 'Heading 2', 'astra' ); ?></span></h2>
-						<div class="inside">
 							<p>
-								<?php _e( 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', 'astra' ); ?>
+								<?php _e( 'Go ahead and start customizing your website.', 'astra' ); ?>
 							</p>
-							<button class="button"><?php _e( 'Button 2', 'astra' ); ?></button>
-						</div>
-				 	</div>
-				</div>
-
-				<div id="normal-sortables-3" class="meta-box-sortables ui-sortable astra-col-33">
-				 	<div class="postbox ">
-						<button type="button" class="handlediv button-link" aria-expanded="true"><span class="screen-reader-text"><?php _e( 'Toggle panel: Clear Cache', 'astra' ); ?></span><span class="toggle-indicator" aria-hidden="true"></span></button>
-						<h2 class="ui-sortable-handle"><span><?php _e( 'Heading 3', 'astra' ); ?></span></h2>
-						<div class="inside">
-							<p>
-								<?php _e( 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', 'astra' ); ?>
-							</p>
-							<button class="button button-primary"><?php _e( 'Button 3', 'astra' ); ?></button>
+							<a class="submit button button-primary" href="<?php echo esc_url( admin_url( 'customize.php' ) ); ?>"><?php _e( 'Customize', 'astra' ); ?></a>
 						</div>
 				 	</div>
 				</div>
