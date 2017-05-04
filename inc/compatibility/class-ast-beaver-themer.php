7e52a1f83c885a42db0d9a9c79e1bd69c739bb6f
dineshchouhan <dineshc@bsf.io>
2017-05-04 14:15:03 +0530

    Themer Supports Part updated

--- inc/compatibility/class-ast-beaver-themer.php
+++ inc/compatibility/class-ast-beaver-themer.php
@@ -95,6 +95,13 @@ if ( ! class_exists( 'Ast_Beaver_Themer' ) ) :
 
 			return array(
 				array(
+					'label' => 'Page',
+					'hooks' => array(
+						'ast_body_top'    => __( 'Before Page', 'astra' ),
+						'ast_body_bottom' => __( 'After Page', 'astra' ),
+					),
+				),
+				array(
 					'label' => 'Header',
 					'hooks' => array(
 						'ast_header_before' => __( 'Before Header', 'astra' ),
@@ -106,6 +113,8 @@ if ( ! class_exists( 'Ast_Beaver_Themer' ) ) :
 					'hooks' => array(
 						'ast_content_before' => __( 'Before Content', 'astra' ),
 						'ast_content_after'  => __( 'After Content', 'astra' ),
+						'ast_content_top'    => __( 'Content Top', 'astra' ),
+						'ast_content_bottom' => __( 'Content Bottom', 'astra' ),
 					),
 				),
 				array(
@@ -113,6 +122,26 @@ if ( ! class_exists( 'Ast_Beaver_Themer' ) ) :
 					'hooks' => array(
 						'ast_footer_before' => __( 'Before Footer', 'astra' ),
 						'ast_footer_after'  => __( 'After Footer', 'astra' ),
+					),
+				),
+				array(
+					'label' => 'Sidebar',
+					'hooks' => array(
+						'ast_sidebars_before' => __( 'Before Sidebar', 'astra' ),
+						'ast_sidebars_after'  => __( 'After Sidebar', 'astra' ),
+					),
+				),
+				array(
+					'label' => 'Posts',
+					'hooks' => array(
+						'loop_start'               => __( 'Loop Start', 'astra' ),
+						'loop_end'                 => __( 'Loop End', 'astra' ),
+						'ast_entry_before'         => __( 'Before Post', 'astra' ),
+						'ast_entry_after'          => __( 'After Post', 'astra' ),
+						'ast_entry_content_before' => __( 'Before Post Content', 'astra' ),
+						'ast_entry_content_after'  => __( 'After Post Content', 'astra' ),
+						'ast_comments_before'      => __( 'Before Comments', 'astra' ),
+						'ast_comments_after'       => __( 'After Comments', 'astra' ),
 					),
 				),
 			);
