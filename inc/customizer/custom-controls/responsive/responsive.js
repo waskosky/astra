d4cbf22ed44d8b1eb063098334e3e9d7187a7192
dineshchouhan <dineshc@bsf.io>
2017-05-23 15:47:14 +0530

    Responsive Param device toggle script updated

--- inc/customizer/custom-controls/responsive/responsive.js
+++ inc/customizer/custom-controls/responsive/responsive.js
@@ -72,7 +72,9 @@
 		},
 
 		astResponsiveInit : function() {
-			jQuery( '.customize-control .ast-responsive-btns button' ).on( 'click', function( event ) {
+			
+			'use strict';
+			this.container.find( '.ast-responsive-btns button' ).on( 'click', function( event ) {
 
 				var device = jQuery(this).attr('data-device');
 				if( 'desktop' == device ) {
@@ -85,13 +87,13 @@
 
 				jQuery( '.wp-full-overlay-footer .devices button[data-device="' + device + '"]' ).trigger( 'click' );
 			});
-
-			jQuery(' .wp-full-overlay-footer .devices button ').on('click', function() {
-
-				var device = jQuery(this).attr('data-device');
-
-				jQuery( '.customize-control-ast-responsive .input-wrapper input, .customize-control .ast-responsive-btns > li' ).removeClass( 'active' );
-				jQuery( '.customize-control-ast-responsive .input-wrapper input.' + device + ', .customize-control .ast-responsive-btns > li.' + device ).addClass( 'active' );
-			});
 		},
+	});
+	
+	jQuery(' .wp-full-overlay-footer .devices button ').on('click', function() {
+
+		var device = jQuery(this).attr('data-device');
+
+		jQuery( '.customize-control-ast-responsive .input-wrapper input, .customize-control .ast-responsive-btns > li' ).removeClass( 'active' );
+		jQuery( '.customize-control-ast-responsive .input-wrapper input.' + device + ', .customize-control .ast-responsive-btns > li.' + device ).addClass( 'active' );
 	});
