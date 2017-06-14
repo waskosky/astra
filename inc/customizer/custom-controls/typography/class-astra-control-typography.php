2ee2e0cad3163631b8c8d059964284b64be548b9
Rushi Jagani <rushi.jagani@gmail.com>
2017-06-14 12:18:46 +0530

    Phpcs fixes for typography param

--- inc/customizer/custom-controls/typography/class-astra-control-typography.php
+++ inc/customizer/custom-controls/typography/class-astra-control-typography.php
@@ -84,18 +84,17 @@ final class Astra_Control_Typography extends WP_Customize_Control {
 		$js_uri  = ASTRA_THEME_URI . 'inc/customizer/custom-controls/typography/';
 
 		wp_enqueue_script( 'astra-typography', $js_uri . 'typography.js', array( 'jquery', 'customize-base' ), ASTRA_THEME_VERSION, true );
-		
 		$astra_typo_localize = array(
-				'inherit' => __('Inherit', 'astra' ),
-				'100'     => __('Thin 100', 'astra' ),
-				'200'     => __('Extra-Light 200', 'astra' ),
-				'300'     => __('Light 300', 'astra' ),
-				'400'     => __('Normal 400', 'astra' ),
-				'500'     => __('Medium 500', 'astra' ),
-				'600'     => __('Semi-Bold 600', 'astra' ),
-				'700'     => __('Bold 700', 'astra' ),
-				'800'     => __('Extra-Bold 800', 'astra' ),
-				'900'     => __('Ultra-Bold 900', 'astra' ),
+				'inherit' => __( 'Inherit', 'astra' ),
+				'100'     => __( 'Thin 100', 'astra' ),
+				'200'     => __( 'Extra-Light 200', 'astra' ),
+				'300'     => __( 'Light 300', 'astra' ),
+				'400'     => __( 'Normal 400', 'astra' ),
+				'500'     => __( 'Medium 500', 'astra' ),
+				'600'     => __( 'Semi-Bold 600', 'astra' ),
+				'700'     => __( 'Bold 700', 'astra' ),
+				'800'     => __( 'Extra-Bold 800', 'astra' ),
+				'900'     => __( 'Ultra-Bold 900', 'astra' ),
 			);
 
 		wp_localize_script( 'astra-typography', 'astraTypo', $astra_typo_localize );
@@ -134,6 +133,7 @@ final class Astra_Control_Typography extends WP_Customize_Control {
 	 * Renders a font control.
 	 *
 	 * @since 1.0.0
+	 * @param  string $default Inherit/Default.
 	 * @access protected
 	 * @return void
 	 */
@@ -165,6 +165,7 @@ final class Astra_Control_Typography extends WP_Customize_Control {
 	 * Renders a font weight control.
 	 *
 	 * @since 1.0.0
+	 * @param  string $default Inherit/Default.
 	 * @access protected
 	 * @return void
 	 */
