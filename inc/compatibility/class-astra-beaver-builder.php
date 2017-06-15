9d3a95c2bff33db7299a5868d12c31b9bf451a74
dineshchouhan <dineshc@bsf.io>
2017-06-15 17:25:05 +0530

    Page builders style combined with Style and removed unwanted files

--- inc/compatibility/class-astra-beaver-builder.php
+++ inc/compatibility/class-astra-beaver-builder.php
@@ -43,20 +43,7 @@ if ( ! class_exists( 'Astra_Beaver_Builder' ) ) :
 		 * Constructor
 		 */
 		public function __construct() {
-			add_filter( 'astra_theme_assets',       array( $this, 'add_styles' ) );
 			add_filter( 'astra_get_content_layout',     array( $this, 'builder_template_content_layout' ), 20 );
-		}
-
-		/**
-		 * Add assets in theme
-		 *
-		 * @param array $assets list of theme assets (JS & CSS).
-		 * @return array List of updated assets.
-		 * @since 1.0.0
-		 */
-		function add_styles( $assets ) {
-			$assets['css']['astra-bb-plugin'] = 'site-compatible/bb-plugin' ;
-			return $assets;
 		}
 
 		/**
