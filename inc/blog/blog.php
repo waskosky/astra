b73afb829c9f77e775664a09cad0080f1d92ac3c
maheshwaghmare <maheshw@bsf.io>
2017-06-26 19:09:50 +0530

    Fixed author schema code issue.

--- inc/blog/blog.php
+++ inc/blog/blog.php
@@ -77,7 +77,7 @@ if ( ! function_exists( 'astra_blog_get_post_meta' ) ) {
 			$output_str = astra_get_post_meta( $post_meta );
 
 			if ( 'post' == get_post_type() && ! empty( $output_str ) ) {
-				echo apply_filters( 'astra_blog_post_meta', '<div class="entry-meta">' . wp_kses_post( $output_str ) . '</div>' );
+				echo apply_filters( 'astra_blog_post_meta', '<div class="entry-meta">' . $output_str . '</div>' );
 			}
 		}
 	}
