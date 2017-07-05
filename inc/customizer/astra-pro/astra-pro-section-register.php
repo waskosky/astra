b8e35c050e23275bf240408a9085b1a34b294f2a
Rushi Jagani <rushi.jagani@gmail.com>
2017-07-05 20:55:23 +0530

    Notice Text,style updated

--- inc/customizer/astra-pro/astra-pro-section-register.php
+++ inc/customizer/astra-pro/astra-pro-section-register.php
@@ -1,19 +1,26 @@
 <?php 
-	/**
-	 * Upgrade to Astra Pro
-	*/
-	// Register custom section types.
-	$wp_customize->register_section_type( 'Astra_Pro_Customizer' );
+/**
+ * Register customizer Aspra Pro Section.
+ *
+ * @package   Astra
+ * @author    Astra
+ * @copyright Copyright (c) 2017, Astra
+ * @link      http://wpastra.com/
+ * @since     Astra 1.0.10
+ */
 
-	// Register sections.
-	$wp_customize->add_section(
-	    new Astra_Pro_Customizer(
-	        $wp_customize,
-	        'astra-pro',
-	        array(
-	            'title'    => esc_html__( 'Get Astra Pro', 'astra' ),
-	            'pro_url'  => 'https://wpastra.com/pro/',
-	            'priority' => 1,
-	        )
-	    )
-	);
+// Register custom section types.
+$wp_customize->register_section_type('Astra_Pro_Customizer');
+
+// Register sections.
+$wp_customize->add_section(
+    new Astra_Pro_Customizer(
+        $wp_customize,
+        'astra-pro',
+        array(
+            'title'    => esc_html__('Get more with Astra Pro', 'astra'),
+            'pro_url'  => 'https://wpastra.com/pro/',
+            'priority' => 1,
+        )
+    )
+);
--- inc/customizer/astra-pro/class-astra-pro-customizer.php
+++ inc/customizer/astra-pro/class-astra-pro-customizer.php
@@ -1,58 +1,65 @@
 <?php
+/**
+* Astra Pro Customizer Section
+*
+* @package   Astra
+* @copyright Copyright (c) 2017, Astra
+* @link      http://wpastra.com/
+* @since     Astra 1.0.10
+*/
+
+class Astra_Pro_Customizer extends WP_Customize_Section
+{
+
     /**
-     * Astra Pro customizer section.
-     *
-     * @since  1.0.0
-     * @access public
-     */
-    class Astra_Pro_Customizer extends WP_Customize_Section {
+    * The type of customize section being rendered.
+    *
+    * @since  1.0.10
+    * @access public
+    * @var    string
+    */
+    public $type     = 'astra-pro';
 
-        /**
-         * The type of customize section being rendered.
-         *
-         * @since  1.0.0
-         * @access public
-         * @var    string
-         */
-        public $type = 'astra-pro';
+    /**
+    * Custom pro button URL.
+    *
+    * @since  1.0.10
+    * @access public
+    * @var    string
+    */
+    public $pro_url  = '';
 
-        /**
-         * Custom pro button URL.
-         *
-         * @since  1.0.0
-         * @access public
-         * @var    string
-         */
-        public $pro_url = '';
-
-        /**
-         * Add custom parameters to pass to the JS via JSON.
-         *
-         * @since  1.0.0
-         * @access public
-         * @return void
-         */
-        public function json() {
-            $json = parent::json();
-            $json['pro_url']  = esc_url( $this->pro_url );
-            return $json;
-        }
-
-        /**
-         * Outputs the Underscore.js template.
-         *
-         * @since  1.0.0
-         * @access public
-         * @return void
-         */
-        protected function render_template() { ?>
-
-            <li id="accordion-section-{{ data.id }}" class="accordion-section control-section control-section-{{ data.type }} cannot-expand control-section-default">
-                <h3>
-                    <# if ( data.title && data.pro_url ) { #>
-                        <a href="{{ data.pro_url }}" target="_blank">{{ data.title }}</a>
-                    <# } #>
-                </h3>
-            </li>
-        <?php }
+    /**
+    * Add custom parameters to pass to the JS via JSON.
+    *
+    * @since  1.0.10
+    * @access public
+    * @return void
+    */
+    public function json() 
+    {
+        $json            = parent::json();
+        $json['pro_url'] = esc_url($this->pro_url);
+        return $json;
     }
+
+    /**
+    * Outputs the Underscore.js template.
+    *
+    * @since  1.0.10
+    * @access public
+    * @return void
+    */
+    protected function render_template() 
+    {
+    ?>
+
+        <li id="accordion-section-{{ data.id }}" class="accordion-section control-section control-section-{{ data.type }} cannot-expand control-section-default">
+         <h3 class="wp-ui-highlight">
+          <# if ( data.title && data.pro_url ) { #>
+           <a href="{{ data.pro_url }}" class="wp-ui-text-highlight" target="_blank">{{ data.title }}</a>
+          <# } #>
+         </h3>
+        </li>
+    <?php }
+}
