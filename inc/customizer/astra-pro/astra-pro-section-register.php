b5f98a1fb89af1c548aac1cc8522667a5d584e64
Rushi Jagani <rushi.jagani@gmail.com>
2017-07-05 21:23:56 +0530

    Phpcs Fixes

--- inc/customizer/astra-pro/astra-pro-section-register.php
+++ inc/customizer/astra-pro/astra-pro-section-register.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * Register customizer Aspra Pro Section.
  *
@@ -10,17 +10,14 @@
  */
 
 // Register custom section types.
-$wp_customize->register_section_type('Astra_Pro_Customizer');
+$wp_customize->register_section_type( 'Astra_Pro_Customizer' );
 
 // Register sections.
 $wp_customize->add_section(
-    new Astra_Pro_Customizer(
-        $wp_customize,
-        'astra-pro',
-        array(
-            'title'    => esc_html__('Get more with Astra Pro', 'astra'),
-            'pro_url'  => esc_url('https://wpastra.com/pro/'),
-            'priority' => 1,
-        )
-    )
+	new Astra_Pro_Customizer( $wp_customize, 'astra-pro', array(
+			'title'    => esc_html__( 'Get more with Astra Pro', 'astra' ),
+			'pro_url'  => esc_url( 'https://wpastra.com/pro/' ),
+			'priority' => 1,
+			)
+	)
 );
--- inc/customizer/astra-pro/class-astra-pro-customizer.php
+++ inc/customizer/astra-pro/class-astra-pro-customizer.php
@@ -1,65 +1,78 @@
 <?php
 /**
-* Astra Pro Customizer Section
-*
-* @package   Astra
-* @copyright Copyright (c) 2017, Astra
-* @link      http://wpastra.com/
-* @since     Astra 1.0.10
-*/
+ * Astra Pro Customizer Section
+ *
+ * @package   Astra
+ * @copyright Copyright (c) 2017, Astra
+ * @link      http://wpastra.com/
+ * @since     Astra 1.0.10
+ */
 
-class Astra_Pro_Customizer extends WP_Customize_Section
-{
+// No direct access, please.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-    /**
-    * The type of customize section being rendered.
-    *
-    * @since  1.0.10
-    * @access public
-    * @var    string
-    */
-    public $type     = 'astra-pro';
+/**
+ * Astra_Pro_Customizer
+ *
+ * @since 1.0.10
+ */
+if ( ! class_exists( 'Astra_Pro_Customizer' ) ) {
 
-    /**
-    * Custom pro button URL.
-    *
-    * @since  1.0.10
-    * @access public
-    * @var    string
-    */
-    public $pro_url  = '';
+	/**
+	 * Astra_Pro_Customizer Initial setup
+	 */
+	class Astra_Pro_Customizer extends WP_Customize_Section {
 
-    /**
-    * Add custom parameters to pass to the JS via JSON.
-    *
-    * @since  1.0.10
-    * @access public
-    * @return void
-    */
-    public function json() 
-    {
-        $json            = parent::json();
-        $json['pro_url'] = esc_url($this->pro_url);
-        return $json;
-    }
+		/**
+		 * The type of customize section being rendered.
+		 *
+		 * @since  1.0.10
+		 * @access public
+		 * @var    string
+		 */
+		public $type     = 'astra-pro';
 
-    /**
-    * Outputs the Underscore.js template.
-    *
-    * @since  1.0.10
-    * @access public
-    * @return void
-    */
-    protected function render_template() 
-    {
-    ?>
+		/**
+		 * Custom pro button URL.
+		 *
+		 * @since  1.0.10
+		 * @access public
+		 * @var    string
+		 */
+		public $pro_url  = '';
 
-        <li id="accordion-section-{{ data.id }}" class="accordion-section control-section control-section-{{ data.type }} cannot-expand control-section-default">
-         <h3 class="wp-ui-highlight">
-          <# if ( data.title && data.pro_url ) { #>
-           <a href="{{ data.pro_url }}" class="wp-ui-text-highlight" target="_blank">{{ data.title }}</a>
-          <# } #>
-         </h3>
-        </li>
-    <?php }
+		/**
+		 * Add custom parameters to pass to the JS via JSON.
+		 *
+		 * @since  1.0.10
+		 * @access public
+		 * @return string
+		 */
+		public function json() {
+			$json            = parent::json();
+			$json['pro_url'] = esc_url( $this->pro_url );
+			return $json;
+		}
+
+		/**
+		 * Outputs the Underscore.js template.
+		 *
+		 * @since  1.0.10
+		 * @access public
+		 * @return void
+		 */
+		protected function render_template() {
+		?>
+		<li id="accordion-section-{{ data.id }}" class="accordion-section control-section control-section-{{ data.type }} cannot-expand control-section-default">
+			<h3 class="wp-ui-highlight">
+				<# if ( data.title && data.pro_url ) { #>
+				<a href="{{ data.pro_url }}" class="wp-ui-text-highlight" target="_blank">{{ data.title }}</a>
+				<# } #>
+			</h3>
+		</li>
+		<?php }
+	}
+
 }
