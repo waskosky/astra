ac5809f211ea06df1ea543ed21f5ea7c4782603f
Nikhil Chavan <email@nikhilchavan.com>
2017-08-11 15:22:50 +0530

    Removed unused variable declaration

--- inc/customizer/custom-controls/typography/class-astra-control-typography.php
+++ inc/customizer/custom-controls/typography/class-astra-control-typography.php
@@ -93,7 +93,6 @@ final class Astra_Control_Typography extends WP_Customize_Control {
 	 */
 	public function enqueue() {
 
-		$css_uri = ASTRA_THEME_URI . 'inc/customizer/custom-controls/typography/';
 		$js_uri  = ASTRA_THEME_URI . 'inc/customizer/custom-controls/typography/';
 
 		wp_enqueue_script( 'astra-typography', $js_uri . 'typography.js', array( 'jquery', 'customize-base' ), ASTRA_THEME_VERSION, true );
