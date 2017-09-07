f6ff97b59cbfe322212bb94033b929bd192e5dcc
Nikhil Chavan <email@nikhilchavan.com>
2017-09-07 14:58:45 +0530

    Removed inline comments which are not required

--- inc/extras.php
+++ inc/extras.php
@@ -1152,7 +1152,6 @@ if ( ! function_exists( 'astra_check_is_ie' ) ) :
 
 		$ua = htmlentities( $_SERVER['HTTP_USER_AGENT'], ENT_QUOTES, 'UTF-8' );
 		if ( strpos( $ua, 'Trident/7.0' ) !== false ) {
-			// do stuff for IE.
 			$is_ie = true;
 		}
 
