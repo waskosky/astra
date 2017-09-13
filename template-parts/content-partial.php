31859b0070a57dc8c0b73dc8971e50916e36e0b3
Sandesh <sandesh2643@gmail.com>
2017-09-13 17:00:03 +0530

    Removed content partial file

--- template-parts/content-partial.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	while ( have_posts() ) : the_post();
-		
-		get_template_part( 'template-parts/content', 'single' );
-
-		// Previous/next post navigation.
-		astra_number_pagination();
-	endwhile;
-?>
