b5488f6660e8b1f0c99c7591d4c7f0b7284ce4d3
Sandesh <sandesh2643@gmail.com>
2017-09-14 09:44:32 +0530

    Svg logo css added for frontend

--- inc/class-astra-dynamic-css.php
+++ inc/class-astra-dynamic-css.php
@@ -184,6 +184,9 @@ if ( ! class_exists( 'Astra_Dynamic_CSS' ) ) {
 				'.site-title' => array(
 					'font-size' => astra_responsive_font( $site_title_font_size, 'desktop' ),
 				),
+				'#masthead .site-logo-img .custom-logo-link img' => array(
+					'max-width' => esc_attr( astra_get_option( 'ast-header-logo-width' ) ) . 'px',
+				),
 				'.ast-archive-description .ast-archive-title' => array(
 					'font-size' => astra_responsive_font( $archive_summary_title_font_size, 'desktop' ),
 				),
@@ -388,15 +391,8 @@ if ( ! class_exists( 'Astra_Dynamic_CSS' ) ) {
 				'.ast-pagination a:hover, .ast-pagination a:focus, .ast-pagination > span:hover:not(.dots), .ast-pagination > span.current, .page-links > .page-link, .page-links .page-link:hover, .post-navigation a:hover' => array(
 					'color' => esc_attr( $link_hover_color ),
 				),
-
 			);
-
-			if ( is_customize_preview() ) {
-				$css_output['#masthead .site-logo-img .custom-logo-link img'] = array(
-					'max-width' => astra_get_option( 'ast-header-logo-width' ) . 'px',
-				);
-			}
-
+			
 			/* Parse CSS from array() */
 			$parse_css = astra_parse_css( $css_output );
 
