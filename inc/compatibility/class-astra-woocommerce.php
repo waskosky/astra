a613e5ca6f9cf190f0a2e6d7d2b6c6beecbf5765
Sandesh <sandesh2643@gmail.com>
2017-12-08 16:10:28 +0530

    Product meta and rating css fixed

--- inc/compatibility/class-astra-woocommerce.php
+++ inc/compatibility/class-astra-woocommerce.php
@@ -356,7 +356,7 @@ if ( ! class_exists( 'Astra_Woocommerce' ) ) :
 					'border-radius'    => astra_get_css_value( $btn_border_radius, 'px' ),
 					'padding'          => astra_get_css_value( $btn_vertical_padding, 'px' ) . ' ' . astra_get_css_value( $btn_horizontal_padding, 'px' ),
 				),
-				'.woocommerce.single-product .star-rating, .woocommerce.single-product .comment-form-rating .stars a' => array(
+				'.woocommerce .star-rating, .woocommerce .comment-form-rating .stars a' => array(
 					'color'			=> $theme_color,
 				),
 				'.woocommerce div.product .woocommerce-tabs ul.tabs li.active a' => array(
