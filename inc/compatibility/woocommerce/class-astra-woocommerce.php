ec0091c6c6ac095bb1e0e6a8616e16329df064fe
Rushi <rushij@bsf.io>
2018-01-19 13:13:18 +0530

    Woocommerce form validated fields border color as theme color

--- inc/compatibility/woocommerce/class-astra-woocommerce.php
+++ inc/compatibility/woocommerce/class-astra-woocommerce.php
@@ -644,7 +644,8 @@ if ( ! class_exists( 'Astra_Woocommerce' ) ) :
 				'.woocommerce ul.products li.product .price, .woocommerce div.product p.price, .woocommerce div.product span.price, .widget_layered_nav_filters ul li.chosen a, .woocommerce-page ul.products li.product .ast-woo-product-category, .wc-layered-nav-rating a' => array(
 					'color' => $text_color,
 				),
-				'.woocommerce nav.woocommerce-pagination ul,.woocommerce nav.woocommerce-pagination ul li' => array(
+				// Form Fields, Pagination border Color.
+				'.woocommerce nav.woocommerce-pagination ul,.woocommerce nav.woocommerce-pagination ul li,.woocommerce form .form-row.woocommerce-validated .select2-container, .woocommerce form .form-row.woocommerce-validated input.input-text, .woocommerce form .form-row.woocommerce-validated select' => array(
 					'border-color' => $theme_color,
 				),
 				'.woocommerce nav.woocommerce-pagination ul li a:focus, .woocommerce nav.woocommerce-pagination ul li a:hover, .woocommerce nav.woocommerce-pagination ul li span.current' => array(
