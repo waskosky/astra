d523bb17b4f9554313dd3df65ed7ed00d9feded8
Sandesh <sandeshj@bsf.io>
2018-01-22 15:27:30 +0530

    Removed repeated code from WooCommerce

--- inc/compatibility/woocommerce/class-astra-woocommerce.php
+++ inc/compatibility/woocommerce/class-astra-woocommerce.php
@@ -334,11 +334,6 @@ if ( ! class_exists( 'Astra_Woocommerce' ) ) :
 			add_theme_support( 'wc-product-gallery-zoom' );
 			add_theme_support( 'wc-product-gallery-lightbox' );
 			add_theme_support( 'wc-product-gallery-slider' );
-
-			add_theme_support( 'wc-product-gallery-zoom' );
-			add_theme_support( 'wc-product-gallery-lightbox' );
-			add_theme_support( 'wc-product-gallery-slider' );
-
 		}
 
 		/**
