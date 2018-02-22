8d213bb59936bd51d883b8e6835a6b152a2bf752
Dinesh Chouhan <dineshc@bsf.io>
2018-02-22 15:49:06 +0530

    Style updated

--- inc/compatibility/learndash/class-astra-learndash.php
+++ inc/compatibility/learndash/class-astra-learndash.php
@@ -102,7 +102,7 @@ if ( ! class_exists( 'Astra_LearnDash' ) ) :
 					'color' => $theme_color,
 				),
 				'.learndash .notcompleted:before, #learndash_profile .notcompleted:before, .learndash_topic_dots ul .topic-notcompleted span:before, .learndash_navigation_lesson_topics_list ul .topic-notcompleted span:before, .learndash .topic-notcompleted span:before' => array(
-					'color' => astra_hex_to_rgba( $text_color, .4 ),
+					'color' => astra_hex_to_rgba( $text_color, .5 ),
 				),
 				'body .thumbnail.course .ld_course_grid_price, body .thumbnail.course .ld_course_grid_price.ribbon-enrolled, body #learndash_lessons #lesson_heading, body #learndash_profile .learndash_profile_heading, body #learndash_quizzes #quiz_heading, body #learndash_lesson_topics_list div > strong' => array(
 					'background-color' => $theme_color,
