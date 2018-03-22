59dba734fd7c7592348aa63ae61e047bfd9b259d
Mahesh Waghmare <mwaghmare7@gmail.com>
2018-03-22 20:28:34 +0530

    - Used astra_content_loop() for 404 page and created content-404.php in template-parts. - Categorized the post, page, search, comment and 404 templates on hook astra_template_parts_content().

--- inc/loop.php
+++ inc/loop.php
@@ -46,53 +46,63 @@ if( ! class_exists( 'Astra_Loop' ) ) :
 		 */
 		public function __construct()
 		{
-			add_action('astra_content_loop', array( $this, 'loop_markup' ) );			
-			add_action('astra_template_parts_content', array( $this, 'template_parts' ) );			
+			add_action('astra_content_loop', array( $this, 'loop_markup' ) );
+
+			add_action('astra_template_parts_content', array( $this, 'template_parts_page' ) );
+			add_action('astra_template_parts_content', array( $this, 'template_parts_post' ) );
+			add_action('astra_template_parts_content', array( $this, 'template_parts_search' ) );
+			add_action('astra_template_parts_content', array( $this, 'template_parts_default' ) );
+			add_action('astra_template_parts_content', array( $this, 'template_parts_comments' ), 15 );
+
 			add_action('astra_template_parts_content_none', array( $this, 'template_parts_none' ) );
+			add_action('astra_template_parts_content_none', array( $this, 'template_parts_404' ) );
 
 			add_action('astra_template_parts_content_top', array( $this, 'template_parts_content_top' ) );
 			add_action('astra_template_parts_content_bottom', array( $this, 'template_parts_content_bottom' ) );
 		}
 
-		function template_parts_content_top() {
-			if( is_archive() ) {
-				astra_content_while_before();
-			}	
-		}
-
-		function template_parts_content_bottom() {
-			if( is_archive() ) {
-				astra_content_while_after();
-			}	
-		}
-
-		/**
-		 * Content Loop
-		 * @since 1.0.0
-		 */
 		function template_parts_none() {
 			if( is_archive() || is_search() ) {
 				get_template_part( 'template-parts/content', 'none' );
+			} 
+		}
+
+		function template_parts_404() {
+			if( is_404() ) {
+				get_template_part( 'template-parts/content', '404' );
 			}
 		}
 
-		function template_parts() {
-			if( is_single() || is_page() ) {
-				if( is_single() ) {
-					get_template_part( 'template-parts/content', 'single' );					
-				} else if( is_page() ) {
-					get_template_part( 'template-parts/content', 'page' );
-				}
+		function template_parts_page()
+		{
+			if( is_page() ) {
+				get_template_part( 'template-parts/content', 'page' );
+			}
+		}
+			
+		function template_parts_post() {
+			if( is_single() ) {
+				get_template_part( 'template-parts/content', 'single' );
+			}
+		}
 
+		function template_parts_search() {
+			if( is_search() ) {
+				get_template_part( 'template-parts/content', 'blog' );
+			}
+		}
+
+		function template_parts_comments() {
+			if( is_single() || is_page() ) {
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
 					comments_template();
 				endif;
+			}
+		}
 
-			} else if( is_search() ) {
-				get_template_part( 'template-parts/content', 'blog' );
-			} else {
-
+		function template_parts_default() {
+			if( ! is_page() && ! is_single() && ! is_search() ) {
 				/*
 				 * Include the Post-Format-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
@@ -104,8 +114,6 @@ if( ! class_exists( 'Astra_Loop' ) ) :
 
 		function loop_markup() {
 			?>
-
-			<!-- single.php -->
 			<main id="main" class="site-main" role="main">
 
 				<?php if ( have_posts() ) : ?>
@@ -133,6 +141,18 @@ if( ! class_exists( 'Astra_Loop' ) ) :
 
 			</main><!-- #main -->
 			<?php
+		}
+
+		function template_parts_content_top() {
+			if( is_archive() ) {
+				astra_content_while_before();
+			}	
+		}
+
+		function template_parts_content_bottom() {
+			if( is_archive() ) {
+				astra_content_while_after();
+			}	
 		}
 
 	}
