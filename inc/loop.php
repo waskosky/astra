1c4a920a2714de58f77525fd4992535019df5ece
Mahesh Waghmare <mwaghmare7@gmail.com>
2018-03-22 17:19:26 +0530

    Used astra_content_loop() in single.php. Added hooks astra_content_while_before and astra_content_while_after into is_archive(). And added the template get_template_part( 'template-parts/content', 'none' ) into the is_archive() condition.

--- inc/loop.php
+++ inc/loop.php
@@ -47,46 +47,66 @@ if( ! class_exists( 'Astra_Loop' ) ) :
 		public function __construct()
 		{
 			add_action('astra_content_loop', array( $this, 'loop_markup' ) );			
+			add_action('astra_template_parts_content', array( $this, 'template_parts' ) );			
+			add_action('astra_template_parts_content_none', array( $this, 'template_parts_none' ) );			
 		}
 
 		/**
 		 * Content Loop
 		 * @since 1.0.0
 		 */
+		function template_parts_none() {
+			if( is_archive() ) {
+				get_template_part( 'template-parts/content', 'none' );
+			}
+		}
+
+		function template_parts() {
+			if( is_single( ) ) {
+				get_template_part( 'template-parts/content', 'single' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			} else {
+
+				/*
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content', astra_get_post_format() );
+			}
+		}
+
 		function loop_markup() {
 			?>
+
+			<!-- single.php -->
 			<main id="main" class="site-main" role="main">
 
 				<?php if ( have_posts() ) : ?>
 
-					<?php /* Start the Loop */ ?>
-
-					<?php astra_content_while_before(); ?>
+					<?php if( is_archive() ) : astra_content_while_before(); endif; ?>
 
 					<div class="ast-row">
 					<?php
 					while ( have_posts() ) :
 						the_post();
-						?>
 
-						<?php
-
-							/*
-							 * Include the Post-Format-specific template for the content.
-							 * If you want to override this in a child theme, then include a file
-							 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-							 */
-							get_template_part( 'template-parts/content', astra_get_post_format() );
+						do_action( 'astra_template_parts_content' );
 						?>
 
 					<?php endwhile; ?>
 					</div>
 
-					<?php astra_content_while_after(); ?>
+					<?php if( is_archive() ) : astra_content_while_after(); endif; ?>
 
 				<?php else : ?>
 
-					<?php get_template_part( 'template-parts/content', 'none' ); ?>
+					<?php do_action( 'astra_template_parts_content_none' ); ?>
 
 				<?php endif; ?>
 
