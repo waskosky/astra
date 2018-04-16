f9153655a8f0c9cc481cd696a2ed1eb10e720721
Rushi <rushij@bsf.io>
2018-04-16 17:10:41 +0530

    WIP: Dropdown menu effect

--- assets/js/unminified/navigation.js
+++ assets/js/unminified/navigation.js
@@ -69,6 +69,83 @@ var toggleClass = function ( el, className ) {
 	}
 };
 
+ //GLOBAL VARIABLES
+  var duration = 15,
+      height, interval, counter, flag = 0;
+  // var button = d.getElementById("button"),
+  //     ease = d.getElementById("easing"),
+  //     element = d.getElementById("element"),
+  //     duration = 15,
+  //     height, interval, counter, flag = 0;
+  
+  //**************************************
+  //FUNCTIONS
+  //#1 this is the transition, it won't work for old IE
+  function addEasing(a) {
+    
+    if (flag === 1) { //check flag
+      a.style.cssText = "transition: height 500ms; -webkit-transition: height 500ms; -moz-transition: height 500ms; -o-transition: height 500ms";
+    } else {
+      a.style.cssText = "transition: none; -webkit-transition: none; -moz-transition: none; -o-transition: none";
+    }
+    
+  }
+
+  //#2 the slideUp
+  function slideUp(a, b) {
+    
+    height = a.offsetHeight; //declare the value of "height" variable
+    counter = height; //declare the value of "counter" variable
+
+    var subtractor = height/10;
+
+    //add CSS3 transition
+    addEasing(a);          
+
+    //disable easing button
+    // ease.disabled = 1;
+
+    //to hide the content of the element
+    a.style.overflow = "hidden";
+
+    //decreasing the height
+    interval = setInterval(function () {
+      
+      counter -= subtractor;            
+      if (counter > 0) {
+        a.style.height = counter + "px";
+      } else {
+        a.style.height = 0;
+        b.disabled = 0;
+        // b.innerHTML = "slideDown";              
+        clearInterval(interval);
+      }
+    }, duration);
+
+  }
+
+  //#3 the slideDown
+  function slideDown(a, b) {
+
+    var adder = height/10; //the height is global variable          
+
+    //iteratively increase the height
+    interval = setInterval(function () {            
+      counter += adder;
+      if (counter < height) {
+        a.style.height = counter + "px";
+      } else {
+        a.style.height = height + "px";
+        b.disabled = 0;
+        // b.innerHTML = "slideUp";
+        //enable easing button              
+        // ease.disabled = 0;
+        clearInterval(interval);
+      }
+    }, duration); 
+    
+  }
+
 // CustomEvent() constructor functionality in Internet Explorer 9 and higher.
 (function () {
 
@@ -217,8 +294,10 @@ var toggleClass = function ( el, className ) {
 							toggleClass( __main_header_all[event_index], 'toggle-on' );
 							toggleClass( menu_toggle_all[event_index], 'toggled' );
 							if ( __main_header_all[event_index].classList.contains( 'toggle-on' ) ) {		
+								 slideDown(__main_header_all[event_index], this);
 								__main_header_all[event_index].style.display = 'block';		
 							} else {		
+								 slideUp(__main_header_all[event_index], this);
 								__main_header_all[event_index].style.display = '';		
 							}
 						break;
