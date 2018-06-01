132f30b77777956af04391f2b81cd498dacee6c5
Rushi <rushij@bsf.io>
2018-06-01 19:44:47 +0530

    Border css property improved and make border color equal for main navigation

--- assets/js/unminified/style.js
+++ assets/js/unminified/style.js
@@ -874,7 +874,6 @@ var toggleClass = function ( el, className ) {
 							toggleClass( menu_toggle_all[event_index], 'toggled' );
 							if ( __main_header_all[event_index].classList.contains( 'toggle-on' ) ) {		
 								__main_header_all[event_index].style.display = 'block';	
-								document.body.classList.remove( "ast-main-header-nav-open" );
 								document.body.classList.add( "ast-main-header-nav-open" );	
 							} else {		
 								__main_header_all[event_index].style.display = '';
