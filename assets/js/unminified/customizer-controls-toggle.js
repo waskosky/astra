9673ca50cbd7dbfb49e68bb13342e9a0bc070843
vikasp <vikasp@bsf.io>
2018-07-26 16:39:25 +0530

    Format depedency code

--- assets/js/unminified/customizer-controls-toggle.js
+++ assets/js/unminified/customizer-controls-toggle.js
@@ -62,26 +62,14 @@
 	 */
 	ASTCustomizerToggles = {
 
-		'astra-settings[display-site-title]' :
-		[
+		'astra-settings[display-site-title]' : [],
 
-		],
+		'astra-settings[display-site-tagline]' : [],
 
-		'astra-settings[display-site-tagline]' :
-		[
+		'astra-settings[ast-header-retina-logo]' :[],
+
+		'custom_logo' : [],
 		
-		],
-
-		'astra-settings[ast-header-retina-logo]' :
-		[
-			
-		],
-
-		'custom_logo' :
-		[
-			
-		],
-
 		/**
 		 * Section - Header
 		 *
@@ -94,95 +82,54 @@
 		// Layout 2 > Right Section > Text / HTML
 		// Layout 2 > Right Section > Search Type
 		// Layout 2 > Right Section > Search Type > Search Box Type.
-		'astra-settings[header-main-rt-section]' :
-		[
-			
-		],
-
-		'astra-settings[hide-custom-menu-mobile]' :
-		[
+		'astra-settings[header-main-rt-section]' : [],
 		
-		],
+
+		'astra-settings[hide-custom-menu-mobile]' :[],
+		
 
 		/**
 		 * Blog
 		 */
-		'astra-settings[blog-width]' :
-		[
+		'astra-settings[blog-width]' :[],
 		
-		],
-		'astra-settings[blog-post-structure]' :
-		[
-
-		],
+		'astra-settings[blog-post-structure]' :[],
 
 		/**
 		 * Blog Single
 		 */
-		 'astra-settings[blog-single-post-structure]' :
-		[
-			
-		],
-		'astra-settings[blog-single-width]' :
-		[
+		 'astra-settings[blog-single-post-structure]' : [],
 		
-		],
-		'astra-settings[blog-single-meta]' :
-		[
-			
-		],
+		'astra-settings[blog-single-width]' : [],
+		
+		'astra-settings[blog-single-meta]' :[], 
+		
 
 		/**
 		 * Small Footer
 		 */
-		'astra-settings[footer-sml-layout]' :
-		[
-			
-		],
-		'astra-settings[footer-sml-section-1]' :
-		[
+		'astra-settings[footer-sml-layout]' : [],
 		
-		],
-		'astra-settings[footer-sml-section-2]' :
-		[
+		'astra-settings[footer-sml-section-1]' :[],
 		
-		],
-
-		'astra-settings[footer-sml-divider]' :
-		[
+		'astra-settings[footer-sml-section-2]' :[],
 		
-		],
-
-		'astra-settings[header-main-sep]' :
-		[
+		'astra-settings[footer-sml-divider]' :[],
 		
-		],
-
-		'astra-settings[disable-primary-nav]' :
-		[
+		'astra-settings[header-main-sep]' :[],
 	
-		],
-
+		'astra-settings[disable-primary-nav]' :[],
+		
 		/**
 		 * Footer Widgets
 		 */
-		'astra-settings[footer-adv]' :
-		[
-			
-		],
-		'astra-settings[shop-archive-width]' :
-		[
+		'astra-settings[footer-adv]' :[],
 		
-		],
-		'astra-settings[mobile-header-logo]' :
-		[
+		'astra-settings[shop-archive-width]' :[],
 		
-		],
-		'astra-settings[different-mobile-logo]' :
-		[
-	
-		],
+		'astra-settings[mobile-header-logo]' :[],
 		
+		'astra-settings[different-mobile-logo]' :[],
 
 	};
 
