c76b267766b6e7e26d0773fcc272f795598b8d65
Balachandar <balachandark@bsf.io>
2018-08-07 11:31:31 +0530

    fix priority and dependency issue iwth site identity typo

--- inc/customizer/configurations/typography/class-astra-header-typo-configs.php
+++ inc/customizer/configurations/typography/class-astra-header-typo-configs.php
@@ -39,8 +39,13 @@ if ( ! class_exists( 'Astra_Header_Typo_Configs' ) ) {
 					'name'     => ASTRA_THEME_SETTINGS . '[divider-section-header-typo-title]',
 					'type'     => 'control',
 					'control'  => 'ast-divider',
-					'required' => array( ASTRA_THEME_SETTINGS . '[display-site-title]', '===', 1 ),
-					'section'  => 'section-primary-header-typo',
+					'required' => array(
+						'conditions' => array(
+							array( ASTRA_THEME_SETTINGS . '[display-site-line]', '==', '1' ),
+							array( ASTRA_THEME_SETTINGS . '[display-sticky-site-line]', '==', '1' ),
+						),
+						'operator'   => 'OR',
+					),					'section'  => 'section-primary-header-typo',
 					'priority' => 5,
 					'title'    => __( 'Site Title', 'astra' ),
 					'settings' => array(),
@@ -56,8 +61,13 @@ if ( ! class_exists( 'Astra_Header_Typo_Configs' ) ) {
 					'section'     => 'section-primary-header-typo',
 					'default'     => astra_get_option( 'font-size-site-title' ),
 					'transport'   => 'postMessage',
-					'required'    => array( ASTRA_THEME_SETTINGS . '[display-site-title]', '==', '1' ),
-					'priority'    => 10,
+					'required' => array(
+						'conditions' => array(
+							array( ASTRA_THEME_SETTINGS . '[display-site-title]', '==', '1' ),
+							array( ASTRA_THEME_SETTINGS . '[display-sticky-site-title]', '==', '1' ),
+						),
+						'operator'   => 'OR',
+					),					'priority'    => 10,
 					'title'       => __( 'Font Size', 'astra' ),
 					'input_attrs' => array(
 						'min' => 0,
@@ -75,8 +85,14 @@ if ( ! class_exists( 'Astra_Header_Typo_Configs' ) ) {
 					'name'     => ASTRA_THEME_SETTINGS . '[divider-section-header-typo-tagline]',
 					'type'     => 'control',
 					'control'  => 'ast-divider',
-					'section'  => 'section-header-typo',
-					'required' => array( ASTRA_THEME_SETTINGS . '[display-site-tagline]', '===', 1 ),
+					'section'  => 'section-primary-header-typo',
+					'required' => array(
+						'conditions' => array(
+							array( ASTRA_THEME_SETTINGS . '[display-site-tagline]', '==', '1' ),
+							array( ASTRA_THEME_SETTINGS . '[display-sticky-site-tagline]', '==', '1' ),
+						),
+						'operator'   => 'OR',
+					),
 					'priority' => 15,
 					'title'    => __( 'Site Tagline', 'astra' ),
 					'settings' => array(),
@@ -89,8 +105,14 @@ if ( ! class_exists( 'Astra_Header_Typo_Configs' ) ) {
 					'name'        => ASTRA_THEME_SETTINGS . '[font-size-site-tagline]',
 					'type'        => 'control',
 					'control'     => 'ast-responsive',
-					'required'    => array( ASTRA_THEME_SETTINGS . '[display-site-tagline]', '===', 1 ),
-					'section'     => 'section-header-typo',
+					'required' => array(
+						'conditions' => array(
+							array( ASTRA_THEME_SETTINGS . '[display-site-tagline]', '==', '1' ),
+							array( ASTRA_THEME_SETTINGS . '[display-sticky-site-tagline]', '==', '1' ),
+						),
+						'operator'   => 'OR',
+					),
+					'section'     => 'section-primary-header-typo',
 					'default'     => astra_get_option( 'font-size-site-tagline' ),
 					'transport'   => 'postMessage',
 					'priority'    => 20,
