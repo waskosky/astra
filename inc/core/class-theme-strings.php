a202c7b0ddbec1323a010036423bf19a7a99c050
Mahesh Waghmare <mwaghmare7@gmail.com>
2018-08-14 13:02:24 +0530

    Removed localizations.

--- inc/core/class-theme-strings.php
+++ inc/core/class-theme-strings.php
@@ -37,8 +37,8 @@ if ( ! function_exists( 'astra_default_strings' ) ) {
 				'string-search-nothing-found'            => __( 'Nothing Found', 'astra' ),
 				'string-search-nothing-found-message'    => __( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'astra' ),
 				'string-full-width-search-message'       => __( 'Start typing and press enter to search', 'astra' ),
-				'string-full-width-search-placeholder'   => __( '', 'astra' ),
-				'string-header-cover-search-placeholder' => __( '', 'astra' ),
+				'string-full-width-search-placeholder'   => '',
+				'string-header-cover-search-placeholder' => '',
 				'string-search-input-placeholder'        => __( 'Search &hellip;', 'astra' ),
 
 				// Comment Template Strings.
