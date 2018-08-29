39a10f462042f0191871c1674b2e6c3de1acdc36
vikasp <vikasp@bsf.io>
2018-08-29 19:41:49 +0530

    Changed sequence for nav menu on modules list

--- inc/core/class-astra-admin-settings.php
+++ inc/core/class-astra-admin-settings.php
@@ -698,6 +698,19 @@ if ( ! class_exists( 'Astra_Admin_Settings' ) ) {
 							),
 						),
 					),
+					'nav-menu'           => array(
+						'title'     => __( 'Nav Menu', 'astra' ),
+						'class'     => 'ast-addon',
+						'title_url' => astra_get_pro_url( 'https://wpastra.com/docs/how-to-white-label-astra/', 'astra-dashboard', 'learn-more', 'welcome-page' ),
+						'links'     => array(
+							array(
+								'link_class'   => 'ast-learn-more',
+								'link_url'     => astra_get_pro_url( 'https://wpastra.com/docs/how-to-white-label-astra/', 'astra-dashboard', 'learn-more', 'welcome-page' ),
+								'link_text'    => __( 'Learn More »', 'astra' ),
+								'target_blank' => true,
+							),
+						),
+					),
 					'transparent-header'    => array(
 						'title'     => __( 'Transparent Header', 'astra' ),
 						'class'     => 'ast-addon',
@@ -847,20 +860,6 @@ if ( ! class_exists( 'Astra_Admin_Settings' ) ) {
 							),
 						),
 					),
-					'nav-menu'           => array(
-						'title'     => __( 'Navigation Menu', 'astra' ),
-						'class'     => 'ast-addon',
-						'title_url' => astra_get_pro_url( 'https://wpastra.com/docs/how-to-white-label-astra/', 'astra-dashboard', 'learn-more', 'welcome-page' ),
-						'links'     => array(
-							array(
-								'link_class'   => 'ast-learn-more',
-								'link_url'     => astra_get_pro_url( 'https://wpastra.com/docs/how-to-white-label-astra/', 'astra-dashboard', 'learn-more', 'welcome-page' ),
-								'link_text'    => __( 'Learn More »', 'astra' ),
-								'target_blank' => true,
-							),
-						),
-					),
-
 				)
 			);
 			?>
