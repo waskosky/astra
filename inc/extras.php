20e49d7c88997611bc4f9035ed42e5e02194a7ff
swapnild <swapnild@bsf.io>
2018-09-19 19:12:20 +0530

    Optimize the Submenu animation style. It is now loaded from the theme and used it for the above & below header with the CSS class.

--- inc/extras.php
+++ inc/extras.php
@@ -660,10 +660,17 @@ if ( ! function_exists( 'astra_primary_navigation_markup' ) ) {
 
 			$submenu_class = apply_filters( 'primary_submenu_border_class', ' submenu-with-border' );
 
+			// Link Pointer Style.
 			$pointer_effect = astra_get_option( 'nav-menu-pointer-effect' );
 			if ( 'none' !== $pointer_effect ) {
 				$submenu_class .= ' ast-link-pointer-style ';
-				$submenu_class .= ' ast-link-pointer-style-' . $pointer_effect . ' ';
+				$submenu_class .= ' ast-link-pointer-style-' . esc_html( $pointer_effect ) . ' ';
+			}
+
+			// Menu Animation.
+			$menu_animation = astra_get_option( 'header-main-submenu-container-animation' );
+			if ( ! empty( $menu_animation ) ) {
+				$submenu_class .= ' astra-menu-animation-' . esc_html( $menu_animation ) . ' ';
 			}
 
 			/**
