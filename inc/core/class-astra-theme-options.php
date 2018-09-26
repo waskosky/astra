fd64eda313629251b2186234b78c5a783577245b
Nikhil <email@nikhilchavan.com>
2018-09-26 21:11:43 +0530

    Remove default settings for options which are removed

--- inc/core/class-astra-theme-options.php
+++ inc/core/class-astra-theme-options.php
@@ -180,8 +180,6 @@ if ( ! class_exists( 'Astra_Theme_Options' ) ) {
 					'header-main-menu-label'           => '',
 					'header-main-menu-align'           => 'inline',
 					'header-main-submenu-container-animation' => 'fade',
-					'nav-menu-pointer-color'           => '',
-					'nav-menu-pointer-thickness'       => 2,                    // Mobile Header.
 					'mobile-header-breakpoint'         => '',
 					'mobile-header-logo'               => '',
 					'mobile-header-logo-width'         => '',
