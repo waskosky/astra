cb8dd098b19f0b357f637110a895cf9c4f20ba3f
Nikhil <email@nikhilchavan.com>
2018-10-04 15:29:20 +0530

    use native gutenberg classes for the metabox field wrappers

--- inc/metabox/class-astra-meta-boxes.php
+++ inc/metabox/class-astra-meta-boxes.php
@@ -208,7 +208,7 @@ if ( ! class_exists( 'Astra_Meta_Boxes' ) ) {
 			 * Option: Sidebar
 			 */
 			?>
-			<div class="site-sidebar-layout-meta-wrap">
+			<div class="site-sidebar-layout-meta-wrap components-base-control__field">
 				<p class="post-attributes-label-wrapper" >
 					<strong> <?php esc_html_e( 'Sidebar', 'astra' ); ?> </strong>
 				</p>
@@ -224,7 +224,7 @@ if ( ! class_exists( 'Astra_Meta_Boxes' ) ) {
 			 * Option: Sidebar
 			 */
 			?>
-			<div class="site-content-layout-meta-wrap">
+			<div class="site-content-layout-meta-wrap components-base-control__field">
 				<p class="post-attributes-label-wrapper" >
 					<strong> <?php esc_html_e( 'Content Layout', 'astra' ); ?> </strong>
 				</p>
@@ -241,7 +241,7 @@ if ( ! class_exists( 'Astra_Meta_Boxes' ) ) {
 			 * Option: Disable Sections - Primary Header, Title, Footer Widgets, Footer Bar
 			 */
 			?>
-			<div class="disable-section-meta-wrap">
+			<div class="disable-section-meta-wrap components-base-control__field">
 				<p class="post-attributes-label-wrapper">
 					<strong> <?php esc_html_e( 'Disable Sections', 'astra' ); ?> </strong>
 				</p>
