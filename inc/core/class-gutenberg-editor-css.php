1508e506a1ee46b79a592bebbee8f64c52063c18
Nikhil <email@nikhilchavan.com>
2018-10-04 14:20:13 +0530

    Add font sizes for the headings

--- inc/core/class-gutenberg-editor-css.php
+++ inc/core/class-gutenberg-editor-css.php
@@ -30,11 +30,13 @@ if ( ! class_exists( 'Gutenberg_Editor_CSS' ) ) :
 			$headings_text_transform     = astra_get_option( 'headings-text-transform' );
 			$single_post_title_font_size = astra_get_option( 'font-size-entry-title' );
 			$body_font_family            = astra_body_font_family();
+			$para_margin_bottom          = astra_get_option( 'para-margin-bottom' );
 			$body_font_weight            = astra_get_option( 'body-font-weight' );
 			$body_font_size              = astra_get_option( 'font-size-body' );
 			$body_line_height            = astra_get_option( 'body-line-height' );
-            $body_text_transform         = astra_get_option( 'body-text-transform' );
-			$box_bg_obj = astra_get_option( 'site-layout-outside-bg-obj' );
+			$body_text_transform         = astra_get_option( 'body-text-transform' );
+			$box_bg_obj                  = astra_get_option( 'site-layout-outside-bg-obj' );
+			$text_color                  = astra_get_option( 'text-color' );
 
 			if ( is_array( $body_font_size ) ) {
 				$body_font_size_desktop = ( isset( $body_font_size['desktop'] ) && '' != $body_font_size['desktop'] ) ? $body_font_size['desktop'] : 15;
@@ -47,8 +49,8 @@ if ( ! class_exists( 'Gutenberg_Editor_CSS' ) ) :
 			$desktop_css = array(
 				'html' => array(
 					'font-size' => astra_get_font_css_value( (int) $body_font_size_desktop * 6.25, '%' ),
-                ),
-                '.gutenberg-editor-page #wpwrap .edit-post-layout__content' => astra_get_background_obj( $box_bg_obj ),
+				),
+				'.gutenberg-editor-page #wpwrap .edit-post-layout__content' => astra_get_background_obj( $box_bg_obj ),
 				'.gutenberg-editor-page #wpwrap .edit-post-layout__content .editor-post-title__block,.gutenberg-editor-page #wpwrap .edit-post-layout__content .editor-default-block-appender,.gutenberg-editor-page #wpwrap .edit-post-layout__content .editor-block-list__block' => array(
 					'max-width' => astra_get_css_value( $site_content_width + 40, 'px' ),
 				),
@@ -69,6 +71,10 @@ if ( ! class_exists( 'Gutenberg_Editor_CSS' ) ) :
 					'font-size'      => astra_responsive_font( $body_font_size, 'desktop' ),
 					'line-height'    => esc_attr( $body_line_height ),
 					'text-transform' => esc_attr( $body_text_transform ),
+					'margin-bottom'  => astra_get_css_value( $para_margin_bottom, 'em' ),
+				),
+				'.gutenberg-editor-page #wpwrap .edit-post-layout__content h1,.gutenberg-editor-page #wpwrap .edit-post-layout__content h2,.gutenberg-editor-page #wpwrap .edit-post-layout__content h3,.gutenberg-editor-page #wpwrap .edit-post-layout__content h4,.gutenberg-editor-page #wpwrap .edit-post-layout__content h5,.gutenberg-editor-page #wpwrap .edit-post-layout__content h6' => array(
+					'color' => esc_attr( $text_color ),
 				),
 			);
 
