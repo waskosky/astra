b652db2b7706ec585f9a305cc1c9593a8d63c14d
Nikhil <email@nikhilchavan.com>
2018-10-04 18:10:13 +0530

    Add basic element styling to gutenberg stylesheet

--- inc/core/class-gutenberg-editor-css.php
+++ inc/core/class-gutenberg-editor-css.php
@@ -31,6 +31,8 @@ if ( ! class_exists( 'Gutenberg_Editor_CSS' ) ) :
 			$single_post_title_font_size = astra_get_option( 'font-size-entry-title' );
 			$body_font_family            = astra_body_font_family();
 			$para_margin_bottom          = astra_get_option( 'para-margin-bottom' );
+			$theme_color                 = astra_get_option( 'theme-color' );
+			$link_color                  = astra_get_option( 'link-color', $theme_color );
 			$body_font_weight            = astra_get_option( 'body-font-weight' );
 			$body_font_size              = astra_get_option( 'font-size-body' );
 			$body_line_height            = astra_get_option( 'body-line-height' );
@@ -50,6 +52,9 @@ if ( ! class_exists( 'Gutenberg_Editor_CSS' ) ) :
 				'html' => array(
 					'font-size' => astra_get_font_css_value( (int) $body_font_size_desktop * 6.25, '%' ),
 				),
+				'.gutenberg-editor-page #wpwrap .edit-post-visual-editor a' => array(
+					'color' => esc_attr( $link_color ),
+				),
 				'.gutenberg-editor-page #wpwrap .edit-post-visual-editor' => astra_get_background_obj( $box_bg_obj ),
 				'.gutenberg-editor-page #wpwrap .edit-post-visual-editor .editor-post-title__block,.gutenberg-editor-page #wpwrap .edit-post-visual-editor .editor-default-block-appender,.gutenberg-editor-page #wpwrap .edit-post-visual-editor .editor-block-list__block' => array(
 					'max-width' => astra_get_css_value( $site_content_width + 40, 'px' ),
@@ -64,11 +69,11 @@ if ( ! class_exists( 'Gutenberg_Editor_CSS' ) ) :
 					'font-family'    => astra_get_css_value( $headings_font_family, 'font' ),
 					'font-weight'    => astra_get_css_value( $headings_font_weight, 'font' ),
 					'text-transform' => esc_attr( $headings_text_transform ),
-                ),
-                '.gutenberg-editor-page #wpwrap .edit-post-visual-editor p,.gutenberg-editor-page #wpwrap .edit-post-visual-editor .editor-block-list__block p' => array(
-					'font-size'      => astra_responsive_font( $body_font_size, 'desktop' ),
 				),
-				'.gutenberg-editor-page #wpwrap .edit-post-visual-editor p,.gutenberg-editor-page #wpwrap .edit-post-visual-editor .editor-block-list__block p, .gutenberg-editor-page #wpwrap .edit-post-visual-editor a' => array(
+				'.gutenberg-editor-page #wpwrap .edit-post-visual-editor p,.gutenberg-editor-page #wpwrap .edit-post-visual-editor .editor-block-list__block p' => array(
+					'font-size' => astra_responsive_font( $body_font_size, 'desktop' ),
+				),
+				'.gutenberg-editor-page #wpwrap .edit-post-visual-editor p,.gutenberg-editor-page #wpwrap .edit-post-visual-editor .editor-block-list__block p, .gutenberg-editor-page #wpwrap .edit-post-visual-editor .wp-block-latest-posts a' => array(
 					'font-family'    => astra_get_font_family( $body_font_family ),
 					'font-weight'    => esc_attr( $body_font_weight ),
 					'font-size'      => astra_responsive_font( $body_font_size, 'desktop' ),
@@ -76,8 +81,15 @@ if ( ! class_exists( 'Gutenberg_Editor_CSS' ) ) :
 					'text-transform' => esc_attr( $body_text_transform ),
 					'margin-bottom'  => astra_get_css_value( $para_margin_bottom, 'em' ),
 				),
-				'.gutenberg-editor-page #wpwrap .edit-post-visual-editor h1,.gutenberg-editor-page #wpwrap .edit-post-visual-editor h2,.gutenberg-editor-page #wpwrap .edit-post-visual-editor h3,.gutenberg-editor-page #wpwrap .edit-post-visual-editor h4,.gutenberg-editor-page #wpwrap .edit-post-visual-editor h5,.gutenberg-editor-page #wpwrap .edit-post-visual-editor h6' => array(
+				'.gutenberg-editor-page #wpwrap .edit-post-visual-editor .editor-post-title__block textarea, .gutenberg-editor-page #wpwrap .edit-post-visual-editor h1,.gutenberg-editor-page #wpwrap .edit-post-visual-editor h2,.gutenberg-editor-page #wpwrap .edit-post-visual-editor h3,.gutenberg-editor-page #wpwrap .edit-post-visual-editor h4,.gutenberg-editor-page #wpwrap .edit-post-visual-editor h5,.gutenberg-editor-page #wpwrap .edit-post-visual-editor h6' => array(
 					'color' => esc_attr( $text_color ),
+				),
+				// Blockquote Text Color.
+				'.gutenberg-editor-page #wpwrap .edit-post-visual-editor blockquote, .gutenberg-editor-page #wpwrap .edit-post-visual-editor blockquote a' => array(
+					'color' => astra_adjust_brightness( $text_color, 75, 'darken' ),
+				),
+				'.gutenberg-editor-page #wpwrap .edit-post-visual-editor blockquote' => array(
+					'border-color' => astra_hex_to_rgba( $link_color, 0.05 ),
 				),
 			);
 
