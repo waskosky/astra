5ed376a1d9a09a41d852319c8ddfe364b63628d8
Rushi <rushij@bsf.io>
2018-10-09 20:04:31 +0530

    Replace wp_register_script/style with wp_enqueue_script/style wherever register of style, script not required

--- inc/customizer/class-astra-customizer.php
+++ inc/customizer/class-astra-customizer.php
@@ -623,7 +623,7 @@ if ( ! class_exists( 'Astra_Customizer' ) ) {
 				$dir        = 'unminified';
 			}
 
-			wp_register_script( 'astra-customizer-preview-js', ASTRA_THEME_URI . 'assets/js/' . $dir . '/customizer-preview' . $js_prefix, array( 'customize-preview' ), null, ASTRA_THEME_VERSION );
+			wp_enqueue_script( 'astra-customizer-preview-js', ASTRA_THEME_URI . 'assets/js/' . $dir . '/customizer-preview' . $js_prefix, array( 'customize-preview' ), null, ASTRA_THEME_VERSION );
 
 			$localize_array = array(
 				'headerBreakpoint'            => astra_header_break_point(),
@@ -631,7 +631,6 @@ if ( ! class_exists( 'Astra_Customizer' ) ) {
 			);
 
 			wp_localize_script( 'astra-customizer-preview-js', 'astraCustomizer', $localize_array );
-			wp_enqueue_script( 'astra-customizer-preview-js' );
 		}
 
 		/**
