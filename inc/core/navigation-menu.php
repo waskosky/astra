6d00d812459e36ced9a17b1d8d23519b1b6912a6
Nikhil <email@nikhilchavan.com>
2018-10-23 10:45:01 +0530

    Fix: The default menu displayed from wp_page_menu() on a new installation is unstyled

---
+++ inc/core/navigation-menu.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Navigation Menu customizations.
+ *
+ * @package     Astra
+ * @author      Astra
+ * @copyright   Copyright (c) 2018, Astra
+ * @link        https://wpastra.com/
+ * @since       Astra x.x.x
+ */
+
+/**
+ * Page menu item classes.
+ *
+ * @since x.x.x
+ *
+ * @param array   $atts {
+ *       The HTML attributes applied to the menu item's `<a>` element, empty strings are ignored.
+ *
+ *     @type string $href The href attribute.
+ * }
+ * @param WP_Post $page         Page data object.
+ * @param int     $depth        Depth of page, used for padding.
+ * @param array   $args         An array of arguments.
+ * @param int     $current_page ID of the current page.
+ *
+ * @return array menu item arguments.
+ */
+function astra_page_menu_item_attributes( $atts, $page, $depth, $args, $current_page ) {
+	if ( 'primary' == $args['theme_location'] || 'above_header_menu' == $args['theme_location'] || 'below_header_menu' == $args['theme_location'] ) {
+		if ( ! isset( $atts['class'] ) ) {
+			$atts['class'] = array();
+		}
+
+		$atts['class'][] = 'sub-menu';
+		$atts['class']   = implode( ' ', $atts['class'] );
+	}
+
+	return $atts;
+}
+
+add_filter( 'page_menu_link_attributes', 'astra_page_menu_item_attributes', 10, 5 );
