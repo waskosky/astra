b8aef75ea8514366f0349779828a0c94ebcda935
premanshu <premanshup@bsf.io>
2019-02-21 12:07:43 +0530

    [PHPCS]

--- inc/extras.php
+++ inc/extras.php
@@ -1690,15 +1690,15 @@ if ( ! function_exists( 'ast_filter_ninja_forms_affiliate_id' ) ) :
 	/**
 	 * Return affiliate id.
 	 *
-	 * @param int  $id affiliate id.
+	 * @param int $id affiliate id.
 	 *
 	 * @return int  $id affiliate id.
 	 */
-	function ast_filter_ninja_forms_affiliate_id( $id ) { 
-	    $id = 1115254;
-	    return $id;
+	function ast_filter_ninja_forms_affiliate_id( $id ) {
+		$id = 1115254;
+		return $id;
 	};
 
 endif;
 
-add_filter( 'ninja_forms_affiliate_id', 'ast_filter_ninja_forms_affiliate_id', 10, 1 ); 
+add_filter( 'ninja_forms_affiliate_id', 'ast_filter_ninja_forms_affiliate_id', 10, 1 );
