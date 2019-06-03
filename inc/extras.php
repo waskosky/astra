75e00103a91cadff5984cec2b79fb2109863ce52
premanshu <premanshup@bsf.io>
2019-06-03 12:13:27 +0530

    [Fix] -> Notice for HTTP_USER_AGENT when it is not set or filtered from firewall.

--- inc/extras.php
+++ inc/extras.php
@@ -1440,9 +1440,11 @@ if ( ! function_exists( 'astra_check_is_ie' ) ) :
 
 		$is_ie = false;
 
-		$ua = htmlentities( $_SERVER['HTTP_USER_AGENT'], ENT_QUOTES, 'UTF-8' );
-		if ( strpos( $ua, 'Trident/7.0' ) !== false ) {
-			$is_ie = true;
+		if ( ! empty( $_SERVER['HTTP_USER_AGENT'] ) ) {
+			$ua = htmlentities( $_SERVER['HTTP_USER_AGENT'], ENT_QUOTES, 'UTF-8' );
+			if ( strpos( $ua, 'Trident/7.0' ) !== false ) {
+				$is_ie = true;
+			}
 		}
 
 		return apply_filters( 'astra_check_is_ie', $is_ie );
