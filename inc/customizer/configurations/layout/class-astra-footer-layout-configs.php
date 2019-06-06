7fe38e41c062d3dcb7e399781ee6f6794638dd88
balachandark <balachandark@bsf.io>
2019-06-06 13:04:43 +0530

    remove unwanted sections

--- inc/customizer/configurations/layout/class-astra-footer-layout-configs.php
+++ inc/customizer/configurations/layout/class-astra-footer-layout-configs.php
@@ -252,34 +252,6 @@ if ( ! class_exists( 'Astra_Footer_Layout_Configs' ) ) {
 				),
 
 				/**
-				 * Option: Footer Bar typo Section heading
-				 */
-				array(
-					'name'     => ASTRA_THEME_SETTINGS . '[footer-bar-typography-heading-divider]',
-					'type'     => 'control',
-					'control'  => 'ast-heading',
-					'section'  => 'section-footer-small',
-					'title'    => __( 'Typography', 'astra-addon' ),
-					'priority' => 47,
-					'settings' => array(),
-				),
-
-				/**
-				 * Option: Footer Bar Typography Group
-				 */
-				array(
-					'name'      => ASTRA_THEME_SETTINGS . '[footer-bar-typography-group]',
-					'default'   => astra_get_option( 'footer-bar-typography-group' ),
-					'type'      => 'control',
-					'control'   => 'ast-settings-group',
-					'title'     => __( 'Content', 'astra' ),
-					'section'   => 'section-footer-small',
-					'transport' => 'postMessage',
-					'priority'  => 47,
-					'required'  => array( ASTRA_THEME_SETTINGS . '[footer-sml-layout]', '!=', 'disabled' ),
-				),
-
-				/**
 				 * Option: Header Width
 				 */
 
