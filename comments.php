71b7fa03480cc43da4db36a3ab60b2d04438e3ed
premanshu <premanshup@bsf.io>
2019-06-07 16:02:59 +0530

    [Fix] -> Astra_SKins class missing when calling function.

--- comments.php
+++ comments.php
@@ -31,7 +31,7 @@ if ( post_password_required() ) {
 				<?php
 				$comments_title = apply_filters(
 					'astra_comment_form_title',
-					( 'modern-skin' === astra_get_selected_skin() ? sprintf( // WPCS: XSS OK.
+					( 'modern-skin' === Astra_Skins::astra_get_selected_skin() ? sprintf( // WPCS: XSS OK.
 						/* translators: 1: number of comments */
 						esc_html( _nx( 'This post has %1$s comments', 'This post has %1$s comments', get_comments_number(), 'comments title', 'astra' ) ),
 						number_format_i18n( get_comments_number() )
