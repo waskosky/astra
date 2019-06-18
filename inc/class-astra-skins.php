66affed6e1858f534f22d7f96443a5a90d877e11
Nikhil Chavan <email@nikhilchavan.com>
2019-06-18 15:36:38 +0530

    Don't set default values with conditions in customizer settings defination

--- inc/class-astra-skins.php
+++ inc/class-astra-skins.php
@@ -31,7 +31,7 @@ if ( ! class_exists( 'Astra_Skins' ) ) {
 			add_filter( 'astra_theme_assets', array( $this, 'add_styles' ), 100 );
 			add_filter( 'astra_attr_ast-comment-meta', array( $this, 'comment_meta_attributes' ) );
 			add_filter( 'astra_comment_avatar_size', array( $this, 'comment_avatar_size' ) );
-			add_filter( 'astra_theme_defaults', array( $this, 'set_default_skin' ) );
+			add_filter( 'astra_theme_defaults', array( $this, 'skin_defaults' ) );
 			add_filter( 'astra_comment_form_title', array( $this, 'comment_form_title' ) );
 		}
 
@@ -97,15 +97,20 @@ if ( ! class_exists( 'Astra_Skins' ) ) {
 		 * @param Array $defaults Array of default customizer settings
 		 * @return Array
 		 */
-		public function set_default_skin( $defaults ) {
-			$defaults['site-content-skin'] = 'modern-skin';
+		public function skin_defaults( $defaults ) {
+			if ( 'classic-skin' === self::astra_get_selected_skin() ) {
+				$defaults['font-size-entry-title']['desktop']           = 30;
+				$defaults['font-size-archive-summary-title']['desktop'] = 40;
+				$defaults['font-size-archive-summary-title']['desktop'] = 40;
+				$defaults['site-sidebar-width']                         = 30;
+			}
 
 			return $defaults;
 		}
 
 		/**
 		 * Change comment form title in case of Classic Skin.
-		 * 
+		 *
 		 * @since x.x.x
 		 * @param String $form_title HTML markup for the Comments Form title.
 		 * @return String
@@ -139,7 +144,7 @@ if ( ! class_exists( 'Astra_Skins' ) ) {
 			// 	}
 			// }
 
-			return apply_filters( 'astra_skin_switch', astra_get_option( 'site-content-skin' ) );
+			return apply_filters( 'astra_skin_switch', astra_get_option( 'site-content-skin', 'modern-skin' ) );
 		}
 	}
 }
