48958919dba15a3d6ffc1a7f06adec9a3afc077d
Nikhil Chavan <email@nikhilchavan.com>
2019-07-01 15:56:52 +0530

    build single post author meta using astra_attr

--- inc/blog/blog-config.php
+++ inc/blog/blog-config.php
@@ -132,10 +132,18 @@ if ( ! function_exists( 'astra_post_author' ) ) {
 
 		ob_start();
 
-		?>
-
-		<span class="posted-by vcard author" itemtype="https://schema.org/Person" itemscope="itemscope" itemprop="author">
-			<?php // Translators: Author Name. ?>
+		echo '<span ';
+			echo astra_attr(
+				'post-meta-author',
+				array(
+					'class'     => 'posted-by vcard author',
+					'itemtype'  => 'https://schema.org/Person',
+					'itemscope' => 'itemscope',
+					'itemprop'  => 'author',
+				)
+			);
+		echo '>';
+			// Translators: Author Name. ?>
 			<a class="url fn n" title="<?php printf( esc_attr__( 'View all posts by %1$s', 'astra' ), get_the_author() ); ?>" 
 				href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>" rel="author" itemprop="url">
 				<span class="author-name" itemprop="name"><?php echo get_the_author(); ?></span>
