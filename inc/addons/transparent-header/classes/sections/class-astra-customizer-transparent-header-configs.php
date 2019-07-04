fb6aae59b21c6a077d97979547b48759d9dcc8bc
Uttam Sharma <uttams@bsf.io>
2019-07-04 11:24:51 +0530

    updated group menu label in trasparent header

--- inc/addons/transparent-header/classes/sections/class-astra-customizer-transparent-header-configs.php
+++ inc/addons/transparent-header/classes/sections/class-astra-customizer-transparent-header-configs.php
@@ -320,7 +320,7 @@ if ( ! class_exists( 'Astra_Customizer_Transparent_Header_Configs' ) ) {
 					'default'   => astra_get_option( 'transparent-header-colors-menu' ),
 					'type'      => 'control',
 					'control'   => 'ast-settings-group',
-					'title'     => __( 'Menu', 'astra' ),
+					'title'     => __( 'Menu / Submenu', 'astra' ),
 					'section'   => 'section-transparent-header',
 					'transport' => 'postMessage',
 					'priority'  => 35,
