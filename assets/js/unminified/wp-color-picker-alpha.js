808b63ce4624563e86da82f2b49ddd447ea60c85
premanshu <premanshup@bsf.io>
2019-07-10 11:43:34 +0530

    [Fix] -> Customizer color picker box shadow issue resolved.

--- assets/js/unminified/wp-color-picker-alpha.js
+++ assets/js/unminified/wp-color-picker-alpha.js
@@ -187,7 +187,7 @@
 						}
 						self.toggler.find( 'span.color-alpha' ).css( {
 							'width'                     : '30px',
-							'height'                    : '24px',
+							'height'                    : '22px',
 							'position'                  : 'absolute',
 							'top'                       : 0,
 							'left'                      : 0,
