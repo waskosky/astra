d1eb7db3739d6d1ac3051adc1e77b1cb808bdb62
premanshu <premanshup@bsf.io>
2019-07-15 16:31:29 +0530

    [Fix] -> SVG for Footer Bar added.

--- inc/customizer/custom-controls/radio-image/class-astra-control-radio-image.php
+++ inc/customizer/custom-controls/radio-image/class-astra-control-radio-image.php
@@ -50,7 +50,7 @@ class Astra_Control_Radio_Image extends WP_Customize_Control {
 		if ( '' === Astra_Control_Radio_Image::$higlight_color ) {
 			Astra_Control_Radio_Image::get_highlight_color();
 			?>
-			<style type="text/css">.ast-radio-img-svg svg path { fill: <?php echo Astra_Control_Radio_Image::$higlight_color; ?> !important }</style>
+			<style type="text/css">.ast-radio-img-svg svg * { fill: <?php echo Astra_Control_Radio_Image::$higlight_color; ?> !important }</style>
 			<?php
 		}
 	}
