8e2a6aec123ccf9fe8e6fdc7bafad475660deb20
balachandark <balachandark@bsf.io>
2019-07-31 12:28:50 +0530

    do not load tranparent header and breadcrumb css if not enabled through customizer or meta options

--- inc/addons/breadcrumbs/dynamic-css/dynamic.css.php
+++ inc/addons/breadcrumbs/dynamic-css/dynamic.css.php
@@ -25,6 +25,10 @@ add_filter( 'astra_dynamic_theme_css', 'astra_breadcrumb_section_dynamic_css' );
  */
 function astra_breadcrumb_section_dynamic_css( $dynamic_css, $dynamic_css_filtered = '' ) {
 
+	if ( 'none' === astra_get_option( 'breadcrumb-position' ) ) {
+		return;
+	}
+
 	$dynamic_css .= astra_parse_css(
 		array(
 			'.ast-breadcrumbs .trail-browse, .ast-breadcrumbs .trail-items, .ast-breadcrumbs .trail-items li' => array(
