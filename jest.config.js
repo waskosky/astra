bbdfe949b84c2e264a6a7e93eedb42778104eb2c
Nikhil Chavan <email@nikhilchavan.com>
2019-08-06 17:34:58 +0530

    Initialize initial e2e tests

---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  setupTestFrameworkScriptFile: "<rootDir>/tests/e2e/inc/setup.js",
+};
---
+++ tests/e2e/inc/setup.js
@@ -0,0 +1,36 @@
+var wpCli = require('node-wp-cli');
+
+beforeEach(() => {
+    cleanupWP();
+});
+
+afterEach(() => {
+    cleanupWP();
+});
+
+
+const cleanupWP = (() => {
+    wpCli.call('option delete astra-settings', {  }, function(err, resp) {
+        if (err) throw err;
+    });
+
+    wpCli.call('theme mod remove custom_logo', {  }, function(err, resp) {
+        if (err) throw err;
+    })
+
+    wpCli.call('option delete site_title', {  }, function(err, resp) {
+        if (err) throw err;
+    })
+
+    wpCli.call('option delete site_icon', {  }, function(err, resp) {
+        if (err) throw err;
+    })
+
+    wpCli.call('option update blogdescription "Astra Test Enviornment"', {  }, function(err, resp) {
+        if (err) throw err;
+    })
+
+    wpCli.call('post delete --all --force', {  }, function(err, resp) {
+        if (err) throw err;
+    })
+});
