380c6861719a331db3ac49424a382c91d3327eaf
balachandark <balachandark@bsf.io>
2019-08-09 12:24:44 +0530

    lifter lms filter not working correctly fixed

--- inc/compatibility/learndash/class-astra-learndash.php
+++ inc/compatibility/learndash/class-astra-learndash.php
@@ -185,9 +185,9 @@ if ( ! class_exists( 'Astra_LearnDash' ) ) :
 			/* Parse CSS from array()*/
 			$css_output .= astra_parse_css( $mobile_typography, '', '544' );
 
-			$dynamic_css .= $css_output;
+			$dynamic_css .= apply_filters( 'astra_theme_learndash_dynamic_css', $css_output );
 
-			return apply_filters( 'astra_theme_learndash_dynamic_css', $css_output );
+			return $dynamic_css;
 		}
 
 		/**
