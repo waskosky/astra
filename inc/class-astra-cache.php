9141ac857151b1536ff3c27563e04f72ce991d46
Nikhil Chavan <email@nikhilchavan.com>
2019-08-22 12:52:48 +0530

    Define the CSS var as empty at first

--- inc/class-astra-cache.php
+++ inc/class-astra-cache.php
@@ -22,7 +22,7 @@ class Astra_Cache {
 	 *
 	 * @var string instance
 	 */
-	private static $dynamic_css_data;
+	private static $dynamic_css_data = '';
 
 	/**
 	 * Asset slug for filename.
@@ -275,7 +275,7 @@ class Astra_Cache {
 	 * @return void
 	 */
 	public function theme_enqueue_styles() {
-		$theme_css_data  = apply_filters( 'astra_dynamic_theme_css', self::$dynamic_css_data );
+		$theme_css_data  = apply_filters( 'astra_dynamic_theme_css', self::$dynamic_css_data = '' );
 
 		// Return if there is no data to add in the css file.
 		if ( empty( $theme_css_data ) ) {
