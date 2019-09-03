c3c972f16bfc579c1ae15c9c1eefe5be1d844ae7
premanshu <premanshup@bsf.io>
2019-09-03 15:32:31 +0530

    fix: Scroll to reply comment not working fix.

--- inc/class-astra-skins.php
+++ inc/class-astra-skins.php
@@ -34,6 +34,7 @@ if ( ! class_exists( 'Astra_Skins' ) ) {
 			add_filter( 'astra_theme_defaults', array( $this, 'skin_defaults' ) );
 			add_filter( 'astra_comment_form_title', array( $this, 'comment_form_title' ) );
 			add_filter( 'astra_comment_form_all_post_type_support', array( $this, 'comment_box_markup_on_page' ) );
+			add_filter( 'astra_comment_form_default_markup', array( $this, 'astra_comment_form_scroll_to_reply' ) );
 			add_filter( 'astra_woocommerce_style_handle', array( $this, 'add_woocommerce_styles' ) );
 		}
 
@@ -88,6 +89,25 @@ if ( ! class_exists( 'Astra_Skins' ) ) {
 			}
 
 			return $attr;
+		}
+
+		/**
+		 * Modify Comment Reply Markup for Scroll on reply.
+		 *
+		 * Conditionally change fieldset and id_form in the comment markup.
+		 *
+		 * @since x.x.x
+		 * @param Array $args HTML attributes for the comments reply markup.
+		 * @return array
+		 */
+		public function astra_comment_form_scroll_to_reply( $args ) {
+			// Fallback to old markup when classic skin.
+			if ( 'classic-skin' === self::astra_get_selected_skin() ) {
+				$args['id_form']       = 'ast-commentform';
+				$args['comment_field'] = '<div class="ast-row comment-textarea"><fieldset class="comment-form-comment"><div class="comment-form-textarea ast-col-lg-12"><label for="comment" class="screen-reader-text">' . esc_html( astra_default_strings( 'string-comment-label-message', false ) ) . '</label><textarea id="comment" name="comment" placeholder="' . esc_attr( astra_default_strings( 'string-comment-label-message', false ) ) . '" cols="45" rows="' . ( 'modern-skin' === Astra_Skins::astra_get_selected_skin() ? '6' : '8' ) . '" aria-required="true"></textarea></div></fieldset></div>';
+			}
+
+			return $args;
 		}
 
 		/**
