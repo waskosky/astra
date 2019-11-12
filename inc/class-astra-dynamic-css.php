716a3f45681ece0a047c3a7f10feed329cbeb657
patilvikasj <patilvikasj@gmail.com>
2019-11-12 17:23:56 +0530

    Modified version number in function comments

--- inc/class-astra-dynamic-css.php
+++ inc/class-astra-dynamic-css.php
@@ -1282,7 +1282,7 @@ if ( ! class_exists( 'Astra_Dynamic_CSS' ) ) {
 		/**
 		 * Check backwards compatibility CSS for loading submenu below the header needs to be added.
 		 *
-		 * @since x.x.x
+		 * @since 2.1.3
 		 * @return boolean true if submenu below header fix is to be loaded, False if not.
 		 */
 		public static function astra_submenu_open_below_header_fix() {
