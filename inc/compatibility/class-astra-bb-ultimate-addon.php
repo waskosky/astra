bfd56f5761b99e5a63bf61bc2312cb07ba05ff1c
premanshu <premanshup@bsf.io>
2019-12-03 14:52:46 +0530

    fix: Line height inherit from base typography line height.

--- inc/compatibility/class-astra-bb-ultimate-addon.php
+++ inc/compatibility/class-astra-bb-ultimate-addon.php
@@ -131,7 +131,8 @@ if ( ! class_exists( 'Astra_BB_Ultimate_Addon' ) ) :
 		 * Button Line Height
 		 */
 		function button_line_height() {
-			$theme_btn_line_height = astra_get_option( 'theme-btn-line-height', 1.85714285714286 );
+			$theme_btn_body_line_height = astra_get_option( 'body-line-height', 1.85714285714286 );
+			$theme_btn_line_height      = astra_get_option( 'theme-btn-line-height', $theme_btn_body_line_height );
 			return $theme_btn_line_height;
 		}
 
