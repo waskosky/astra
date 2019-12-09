595ac5004df11b12e964524d253fb7c614129914
Balachandar <balachandark@bsf.io>
2019-12-09 12:29:24 +0530

    fix: global button font size responsive option not working on frontend

--- inc/class-astra-dynamic-css.php
+++ inc/class-astra-dynamic-css.php
@@ -612,7 +612,7 @@ if ( ! class_exists( 'Astra_Dynamic_CSS' ) ) {
 					'padding'             => astra_get_css_value( $btn_vertical_padding, 'px' ) . ' ' . astra_get_css_value( $btn_horizontal_padding, 'px' ),
 					'font-family'         => astra_get_font_family( $theme_btn_font_family ),
 					'font-weight'         => esc_attr( $theme_btn_font_weight ),
-					'font-size'           => astra_responsive_font( $theme_btn_font_size, 'desktop' ),
+					'font-size'           => astra_get_font_css_value( $theme_btn_font_size['desktop'], $theme_btn_font_size['desktop-unit'] ),
 					'line-height'         => esc_attr( $theme_btn_line_height ),
 					'text-transform'      => esc_attr( $theme_btn_text_transform ),
 					'letter-spacing'      => astra_get_css_value( $theme_btn_letter_spacing, 'px' ),
@@ -628,6 +628,23 @@ if ( ! class_exists( 'Astra_Dynamic_CSS' ) ) {
 			/* Parse CSS from array() */
 			$parse_css .= astra_parse_css( $global_button_desktop );
 
+			$global_button_tablet = array(
+				'.menu-toggle, button, .ast-button, .button, input#submit, input[type="button"], input[type="submit"], input[type="reset"]' => array(
+					'font-size' => astra_get_font_css_value( $theme_btn_font_size['tablet'], $theme_btn_font_size['tablet-unit'] ),
+				),
+			);
+
+			/* Parse CSS from array() */
+			$parse_css .= astra_parse_css( $global_button_tablet, '', '768' );
+
+			$global_button_mobile = array(
+				'.menu-toggle, button, .ast-button, .button, input#submit, input[type="button"], input[type="submit"], input[type="reset"]' => array(
+					'font-size' => astra_get_font_css_value( $theme_btn_font_size['mobile'], $theme_btn_font_size['mobile-unit'] ),
+				),
+			);
+
+			/* Parse CSS from array() */
+			$parse_css .= astra_parse_css( $global_button_mobile, '', '544' );
 			/**
 			 * Elementor & Gutenberg button backward compatibility for default styling.
 			 */
