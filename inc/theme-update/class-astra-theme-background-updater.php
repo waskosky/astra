5da6c7391ad642a3f95bffe0b1ab6bd6a90c18e0
Balachandar <balachandark@bsf.io>
2020-01-07 19:42:37 +0530

    remove extra brackets

--- inc/theme-update/class-astra-theme-background-updater.php
+++ inc/theme-update/class-astra-theme-background-updater.php
@@ -74,7 +74,7 @@ if ( ! class_exists( 'Astra_Theme_Background_Updater' ) ) {
 			}
 
 			$is_queue_running = astra_get_option( 'is_theme_queue_running', false );
-			if ( ( $this->needs_db_update() && ! $is_queue_running ) ) {
+			if ( $this->needs_db_update() && ! $is_queue_running ) {
 				$this->update();
 			} else {
 				if ( ! $is_queue_running ) {
