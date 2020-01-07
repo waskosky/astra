6fa6a54d83def8c72d16e47a18911d261c0da987
Uttam Sharma <uttams@bsf.io>
2020-01-07 12:49:47 +0530

    Fixed clear button height issue

--- assets/js/unminified/wp-color-picker-alpha.js
+++ assets/js/unminified/wp-color-picker-alpha.js
@@ -200,7 +200,7 @@
 
 						self.toggler.find( 'span.color-alpha' ).css( {
 							'width'                     : '30px',
-							'height'                    : '24px',
+							'height'                    : '28px',
 							'position'                  : 'absolute',
 							'top'                       : 0,
 							'left'                      : 0,
