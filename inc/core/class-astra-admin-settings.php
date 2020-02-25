8edeff98688454833b81065b93668eb3e7b4081b
ShubhamW <shubhamw@bsf.io>
2020-02-25 12:40:44 +0530

    wp_kses for svq logo

--- inc/core/class-astra-admin-settings.php
+++ inc/core/class-astra-admin-settings.php
@@ -1515,11 +1515,24 @@ if ( ! class_exists( 'Astra_Admin_Settings' ) ) {
 		 */
 		public static function top_header_right_section() {
 
+			$allowed_html = array(
+				'svg'  => array(
+					'width'   => array(),
+					'height'  => array(),
+					'xmlns'   => array(),
+					'viewBox' => array(),
+				),
+				'path' => array(
+					'fill' => array(),
+					'd'    => array(),
+				),
+			);
+
 			$top_links = apply_filters(
 				'astra_header_top_links',
 				array(
 					'astra-theme-info' => array(
-						'title' => __( '<img draggable="false" role="img" class="emoji" alt="Lightening Fast Icon" src="https://s.w.org/images/core/emoji/12.0.0-1/svg/26a1.svg"> Lightning Fast & Fully Customizable WordPress theme!', 'astra' ),
+						'title' => __( '<svg width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" viewBox="-50 -50 100 100"><path fill="#FFAC33" d="M32.938 15.651C32.792 15.26 32.418 15 32 15H19.925L26.89 1.458c.219-.426.106-.947-.271-1.243C26.437.071 26.218 0 26 0c-.233 0-.466.082-.653.243L18 6.588 3.347 19.243c-.316.273-.43.714-.284 1.105S3.582 21 4 21h12.075L9.11 34.542c-.219.426-.106.947.271 1.243.182.144.401.215.619.215.233 0 .466-.082.653-.243L18 29.412l14.653-12.655c.317-.273.43-.714.285-1.106z"/></svg> Lightning Fast & Fully Customizable WordPress theme!', 'astra' ),
 					),
 				)
 			);
@@ -1536,7 +1549,7 @@ if ( ! class_exists( 'Astra_Admin_Settings' ) ) {
 								isset( $info['url'] ) ? 'a' : 'span',
 								isset( $info['url'] ) ? 'href="' . esc_url( $info['url'] ) . '"' : '', // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 								isset( $info['url'] ) ? 'target="_blank" rel="noopener"' : '', // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-								esc_html( $info['title'] )
+								wp_kses( $info['title'], $allowed_html, $allowed_protocols = array() )
 							);
 						}
 						?>
